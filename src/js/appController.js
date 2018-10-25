a3ff6163702270df32ffd9699f9f9d47ef82b9fb
Byron Bai <byron.bai@aol.com>
2018-10-25 09:35:16 +0800

    update

--- src/js/appController.js
+++ src/js/appController.js
@@ -67,34 +67,35 @@ define(['ojs/ojcore', 'knockout', 'ojs/ojmodule-element-utils', 'ojs/ojmodule-el
 
       // Navigation setup
       var navData = [
+        // {
+        //   name: 'Collections', id: 'collections',
+        //   iconClass: 'oj-navigationlist-item-icon demo-icon-font-24 demo-chart-icon-24'
+        // },
+        // {
+        //   name: 'Controls', id: 'controls',
+        //   iconClass: 'oj-navigationlist-item-icon demo-icon-font-24 demo-fire-icon-24'
+        // }, {
+        //   name: 'Forms', id: 'forms',
+        //   iconClass: 'oj-navigationlist-item-icon demo-icon-font-24 demo-people-icon-24'
+        // }, {
+        //   name: 'Framework', id: 'framework',
+        //   iconClass: 'oj-navigationlist-item-icon demo-icon-font-24 demo-info-icon-24'
         {
-          name: 'Collections', id: 'collections',
-          iconClass: 'oj-navigationlist-item-icon demo-icon-font-24 demo-chart-icon-24'
-        }, {
-          name: 'Controls', id: 'controls',
-          iconClass: 'oj-navigationlist-item-icon demo-icon-font-24 demo-fire-icon-24'
-        }, {
-          name: 'Forms', id: 'forms',
+          name: 'Custom Dashboard', id: 'layout',
           iconClass: 'oj-navigationlist-item-icon demo-icon-font-24 demo-people-icon-24'
         }, {
-          name: 'Framework', id: 'framework',
-          iconClass: 'oj-navigationlist-item-icon demo-icon-font-24 demo-info-icon-24'
-        }, {
-          name: 'Layout & Nav', id: 'layout',
-          iconClass: 'oj-navigationlist-item-icon demo-icon-font-24 demo-info-icon-24'
-        }, {
-          name: 'Patterns', id: 'patterns',
+          name: 'Data Visualization Designer', id: 'patterns',
           iconClass: 'oj-navigationlist-item-icon demo-icon-font-24 demo-info-icon-24'
         }, {
           name: 'Visualizations', id: 'visualizations',
-          iconClass: 'oj-navigationlist-item-icon demo-icon-font-24 demo-info-icon-24'
+          iconClass: 'oj-navigationlist-item-icon demo-icon-font-24 demo-chart-icon-24'
         }
       ];
       self.navDataSource = new oj.ArrayTableDataSource(navData, {idAttribute: 'id'});
 
       // Header
       // Application Name used in Branding Area
-      self.appName = ko.observable("JET Playground");
+      self.appName = ko.observable("CDP Dashboard Spike Demo");
       // User Info used in Global Navigation area
       self.userLogin = ko.observable("byron.bai@oracle.com");
     }
